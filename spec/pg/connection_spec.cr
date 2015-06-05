cafe05c806e601a43514c523cd84d75aaa8c3107
Will Leinweber <will@bitfission.com>
2015-06-05 10:06:05 -0700

    Use the temproary global trick instead,
    since eventually it will be fixed in crystal
    
    Revert "Change DB constant to global to work around https://github.com/manastech/crystal/issues/708"
    
    This reverts commit 574f4305f878c0682ccff5acc794721c422f1b25.

--- spec/pg/connection_spec.cr
+++ spec/pg/connection_spec.cr
@@ -10,51 +10,51 @@ end
 
 describe PG::Connection, "#exec untyped" do
   it "returns a Result" do
-    res = $DB.exec("select 1")
+    res = DB.exec("select 1")
     res.class.should eq(PG::Result(Array(PG::PGValue)))
   end
 
   it "raises on bad queries" do
-    expect_raises(PG::ResultError) { $DB.exec("select nocolumn from notable") }
+    expect_raises(PG::ResultError) { DB.exec("select nocolumn from notable") }
   end
 
   it "returns a Result when create table" do
-    res = $DB.exec("create table if not exists test()")
+    res = DB.exec("create table if not exists test()")
     res.class.should eq(PG::Result(Array(PG::PGValue)))
-    $DB.exec("drop table test")
+    DB.exec("drop table test")
   end
 end
 
 describe PG::Connection, "#exec typed" do
   it "returns a Result" do
-    res = $DB.exec({Int32}, "select 1")
+    res = DB.exec({Int32}, "select 1")
     res.class.should eq( PG::Result({Int32.class}) )
   end
 
   it "raises on bad queries" do
-    expect_raises(PG::ResultError) { $DB.exec({Int32}, "select nocolumn from notable") }
+    expect_raises(PG::ResultError) { DB.exec({Int32}, "select nocolumn from notable") }
   end
 end
 
 describe PG::Connection, "#exec typed with params" do
   it "returns a Result" do
-    res = $DB.exec({Float64}, "select $1::float * $2::float ", [3.4, -2])
+    res = DB.exec({Float64}, "select $1::float * $2::float ", [3.4, -2])
     res.class.should eq( PG::Result({Float64.class}) )
   end
 
   it "raises on bad queries" do
-    expect_raises(PG::ResultError) { $DB.exec("select $1::text from notable", ["hello"]) }
+    expect_raises(PG::ResultError) { DB.exec("select $1::text from notable", ["hello"]) }
   end
 end
 
 describe PG::Connection, "#exec untyped with params" do
   it "returns a Result" do
-    res = $DB.exec("select $1::text, $2::text, $3::text", ["hello", "", "world"])
+    res = DB.exec("select $1::text, $2::text, $3::text", ["hello", "", "world"])
     res.class.should eq(PG::Result(Array(PG::PGValue)))
   end
 
   it "raises on bad queries" do
-    expect_raises(PG::ResultError) { $DB.exec("select $1::text from notable", ["hello"]) }
+    expect_raises(PG::ResultError) { DB.exec("select $1::text from notable", ["hello"]) }
   end
 
   it "can properly encode various types" do
@@ -63,7 +63,7 @@ describe PG::Connection, "#exec untyped with params" do
     query = "select
              $1::text, $2::int, $3::text, $4::float, $5::timestamptz, $6::date, $7::bool"
     param =  ["hello",       2,    nil,    -4.23,      time,            date,       true]
-    res = $DB.exec(query, param)
+    res = DB.exec(query, param)
     res.rows.should eq([param])
   end
 
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,10 +3,11 @@ require "../src/pg"
 
 DB_URL = ENV["DATABASE_URL"]? || "postgres:///"
 $DB = PG.connect(DB_URL)
+DB = $DB
 
 module Helper
   def self.db_version_gte(major, minor, patch=0)
-    ver = $DB.version
+    ver = DB.version
     ver[:major] >= major && ver[:minor] >= minor && ver[:patch] >= patch
   end
 end
