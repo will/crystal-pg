cafe3c7b083b9fcfa1527381137593d810dd360e
Will Leinweber <will@bitfission.com>
2015-06-27 09:18:17 -0700

    alais the pointers once instead of making them pointers each time

--- src/pg/libpq.cr
+++ src/pg/libpq.cr
@@ -6,19 +6,19 @@ module PG
     alias Int   = Int32
     alias Oid   = Int32
 
-    alias PGconn = Void
+    alias PGconn = Void*
     enum ConnStatusType
       CONNECTION_OK, CONNECTION_BAD, CONNECTION_STARTED, CONNECTION_MADE,
       CONNECTION_AWAITING_RESPONSE, CONNECTION_AUTH_OK, CONNECTION_SETENV,
       CONNECTION_SSL_STARTUP, CONNECTION_NEEDED
     end
-    fun connect       = PQconnectdb(conninfo : CChar*) : PGconn*
-    fun status        = PQstatus(conn : PGconn*)       : ConnStatusType
-    fun finish        = PQfinish(conn : PGconn*)       : Void
-    fun error_message = PQerrorMessage(conn : PGconn*) : CChar*
-    fun exec          = PQexec(conn : PGconn*, query : CChar*) : PGresult*
+    fun connect       = PQconnectdb(conninfo : CChar*) : PGconn
+    fun status        = PQstatus(conn : PGconn)       : ConnStatusType
+    fun finish        = PQfinish(conn : PGconn)       : Void
+    fun error_message = PQerrorMessage(conn : PGconn) : CChar*
+    fun exec          = PQexec(conn : PGconn, query : CChar*) : PGresult
     fun exec_params   = PQexecParams(
-                          conn          : PGconn* ,
+                          conn          : PGconn ,
                           query         : CChar*  ,
                           n_params      : Int     ,
                           param_types   : Oid*    ,
@@ -26,22 +26,22 @@ module PG
                           param_lengths : Int*    ,
                           param_formats : Int*    ,
                           result_format : Int
-                        ) : PGresult*
+                        ) : PGresult
 
-    alias PGresult = Void
+    alias PGresult = Void*
     enum ExecStatusType
       PGRES_EMPTY_QUERY, PGRES_COMMAND_OK, PGRES_TUPLES_OK, PGRES_COPY_OUT,
       PGRES_COPY_IN, PGRES_BAD_RESPONSE, PGRES_NONFATAL_ERROR, PGRES_FATAL_ERROR,
       PGRES_COPY_BOTH, PGRES_SINGLE_TUPLE
     end
-    fun result_status        = PQresultStatus(res : PGresult*) : ExecStatusType
-    fun result_error_message = PQresultErrorMessage(res : PGresult*) : CChar*
-    fun clear    = PQclear(res : PGresult* )  : Void
-    fun nfields  = PQnfields(res : PGresult*) : Int
-    fun ntuples  = PQntuples(res : PGresult*) : Int
-    fun fname    = PQfname(res : PGresult*, column_number : Int) : CChar*
-    fun ftype    = PQftype(res : PGresult*, column_number : Int) : Int
-    fun getvalue = PQgetvalue(res : PGresult*, row_number : Int, column_number : Int) : CChar*
-    fun getisnull = PQgetisnull(res : PGresult*, row_number : Int, column_number : Int) : Bool
+    fun result_status        = PQresultStatus(res : PGresult) : ExecStatusType
+    fun result_error_message = PQresultErrorMessage(res : PGresult) : CChar*
+    fun clear    = PQclear(res : PGresult )  : Void
+    fun nfields  = PQnfields(res : PGresult) : Int
+    fun ntuples  = PQntuples(res : PGresult) : Int
+    fun fname    = PQfname(res : PGresult, column_number : Int) : CChar*
+    fun ftype    = PQftype(res : PGresult, column_number : Int) : Int
+    fun getvalue = PQgetvalue(res : PGresult, row_number : Int, column_number : Int) : CChar*
+    fun getisnull = PQgetisnull(res : PGresult, row_number : Int, column_number : Int) : Bool
   end
 end
