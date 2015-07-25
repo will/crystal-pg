cafe4daf2f9f7ceec638db9035b9e9688ae085b5
Will Leinweber <will@bitfission.com>
2015-07-25 15:44:24 +0200

    make a new type alias for c strings

--- src/pg/libpq.cr
+++ src/pg/libpq.cr
@@ -3,6 +3,7 @@ module PG
   lib LibPQ
     # http://www.postgresql.org/docs/9.4/static/libpq-exec.html
     alias CChar = UInt8
+    alias CStr  = CChar*
     alias Int   = Int32
     alias Oid   = Int32
 
@@ -12,22 +13,22 @@ module PG
       CONNECTION_AWAITING_RESPONSE, CONNECTION_AUTH_OK, CONNECTION_SETENV,
       CONNECTION_SSL_STARTUP, CONNECTION_NEEDED
     end
-    fun connect       = PQconnectdb(conninfo : CChar*) : PGconn
+    fun connect       = PQconnectdb(conninfo : CStr)  : PGconn
     fun status        = PQstatus(conn : PGconn)       : ConnStatusType
     fun finish        = PQfinish(conn : PGconn)       : Void
-    fun error_message = PQerrorMessage(conn : PGconn) : CChar*
-    fun exec          = PQexec(conn : PGconn, query : CChar*) : PGresult
+    fun error_message = PQerrorMessage(conn : PGconn) : CStr
+    fun exec          = PQexec(conn : PGconn, query : CStr) : PGresult
     fun exec_params   = PQexecParams(
-                          conn          : PGconn ,
-                          query         : CChar*  ,
+                          conn          : PGconn  ,
+                          query         : CStr    ,
                           n_params      : Int     ,
                           param_types   : Oid*    ,
-                          param_values  : CChar** ,
+                          param_values  : CStr*   ,
                           param_lengths : Int*    ,
                           param_formats : Int*    ,
                           result_format : Int
                         ) : PGresult
-    fun escape_literal = PQescapeLiteral(conn : PGconn, str : CChar*, length : Int) : CChar*
+    fun escape_literal = PQescapeLiteral(conn : PGconn, str : CStr, length : Int) : CStr
 
     alias PGresult = Void*
     enum ExecStatusType
@@ -36,13 +37,13 @@ module PG
       PGRES_COPY_BOTH, PGRES_SINGLE_TUPLE
     end
     fun result_status        = PQresultStatus(res : PGresult) : ExecStatusType
-    fun result_error_message = PQresultErrorMessage(res : PGresult) : CChar*
+    fun result_error_message = PQresultErrorMessage(res : PGresult) : CStr
     fun clear    = PQclear(res : PGresult )  : Void
     fun nfields  = PQnfields(res : PGresult) : Int
     fun ntuples  = PQntuples(res : PGresult) : Int
-    fun fname    = PQfname(res : PGresult, column_number : Int) : CChar*
+    fun fname    = PQfname(res : PGresult, column_number : Int) : CStr
     fun ftype    = PQftype(res : PGresult, column_number : Int) : Int
-    fun getvalue = PQgetvalue(res : PGresult, row_number : Int, column_number : Int) : CChar*
+    fun getvalue = PQgetvalue(res : PGresult, row_number : Int, column_number : Int) : CStr
     fun getisnull = PQgetisnull(res : PGresult, row_number : Int, column_number : Int) : Bool
 
     fun freemem = PQfreemem(ptr : Void*) : Void
