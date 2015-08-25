cafe555ec953df1737f466f99a98cbc40c8f11d4
Will Leinweber <will@bitfission.com>
2015-08-24 20:47:58 -0700

    make the decoders pluggable

--- src/pg/decoder.cr
+++ src/pg/decoder.cr
@@ -8,33 +8,6 @@ module PG
   alias PGValue = String | Nil | Bool | Int32 | Float32 | Float64 | Time | JSON::Type
 
   module Decoder
-    # https://github.com/postgres/postgres/blob/master/src/include/catalog/pg_type.h
-    def self.from_oid(oid)
-      case oid
-      when 16
-        BoolDecoder
-      when 20, 21, 23 # 20:int8, 21:int2, 23:int4
-        IntDecoder
-      when 25 # text
-        DefaultDecoder
-      when 114
-        JsonDecoder
-      when 3802
-        JsonbDecoder
-      when 700 # float4
-        Float32Decoder
-      when 701 # float8
-        Float64Decoder
-      when 705 # unknown
-        DefaultDecoder
-      when 1082
-        DateDecoder
-      when 1082, 1114, 1184 # 1082:date 1114:ts, 1184:tstz
-        TimeDecoder
-      else
-        DefaultDecoder
-      end.new
-    end
 
     abstract class Decoder
       def decode(value_ptr) end
@@ -75,10 +48,6 @@ module PG
           false
         when 1
           true
-      #  when 't'.ord
-      #    true
-      #  when 'f'.ord
-      #    false
         else
           raise "bad boolean decode: #{value_ptr.value}"
         end
@@ -131,61 +100,30 @@ module PG
       end
     end
 
-    class StringTimeDecoder < Decoder
-      def decode(value_ptr)
-        curr = value_ptr
+    @@decoders = Hash(Int32, PG::Decoder::Decoder).new
+    @@default = DefaultDecoder.new
 
-        curr, year   = get_next_int(curr)
-        curr, month  = get_next_int(curr)
-        curr, day    = get_next_int(curr)
-        curr, hour   = get_next_int(curr)
-        curr, minute = get_next_int(curr)
-        curr, second = get_next_int(curr)
-        if (curr-1).value == '.'.ord
-          curr, fraction = get_next_int(curr)
-        else
-          fraction = 0
-        end
-        sign = (curr-1).value == '-'.ord ? -1 : 1
-        curr, offset = get_next_int(curr)
-        milisecond = fraction_to_mili(fraction)
-
-        t = Time.new(year, month, day, hour, minute, second, milisecond, Time::Kind::Utc)
-
-        return apply_offset(t, offset*sign)
-      end
-
-      private def get_next_int(curr)
-        return curr, 0 if curr.value == 0
-        int = 0
-        while curr.value >= 48 && curr.value <= 57
-          int = (int*10) + (curr.value - 48)
-          curr += 1
-        end
-        curr += 1 unless curr.value == 0
-        return curr,int
-      end
-
-      # Postgres returns microseconds, Crystal Time only supports miliseconds
-      private def fraction_to_mili(frac)
-        if frac < 10
-          frac * 100
-        elsif frac < 100
-          frac * 10
-        elsif frac > 1000
-          frac / 1000
-        else
-          frac
-        end
-      end
-
-      private def apply_offset(t, offset)
-        if offset == 0
-          t
-        else
-          t - TimeSpan.new(offset,0,0)
-        end
-      end
+    def self.from_oid(oid)
+      @@decoders[oid]? || @@default
     end
+
+    def self.register_decoder(decoder, oid)
+      @@decoders[oid] = decoder
+    end
+
+    # https://github.com/postgres/postgres/blob/master/src/include/catalog/pg_type.h
+    register_decoder BoolDecoder.new,      16 # bool
+    register_decoder IntDecoder.new,       20 # int8
+    register_decoder IntDecoder.new,       21 # int2
+    register_decoder IntDecoder.new,       23 # int4
+    register_decoder DefaultDecoder.new,   25 # text
+    register_decoder JsonDecoder.new,     114 # json
+    register_decoder JsonbDecoder.new,   3802 # jsonb
+    register_decoder Float32Decoder.new,  700 # float4
+    register_decoder Float64Decoder.new,  701 # float8
+    register_decoder DefaultDecoder.new,  705 # unknown
+    register_decoder DateDecoder.new,    1082 # date
+    register_decoder TimeDecoder.new,    1114 # timestamp
+    register_decoder TimeDecoder.new,    1184 # timestamptz
   end
 end
