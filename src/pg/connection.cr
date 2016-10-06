505cfac4b224ee48b905ad62c89700f68cef9e00
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-10-06 13:33:57 -0300

    Removed "SET extra_float_digits = 3" (not needed in binary protocol)

--- src/pg/connection.cr
+++ src/pg/connection.cr
@@ -9,7 +9,6 @@ module PG
       conn_info = PQ::ConnInfo.new(database.uri)
       @connection = PQ::Connection.new(conn_info)
       @connection.connect
-      exec_all("SET extra_float_digits = 3")
     end
 
     def build_statement(query)
