39664d219ab5f95477a55025eb84dfdb75d44392
Marwan Rabbâa <waghanza@gmail.com>
2017-04-12 09:24:40 +0200

    add authentication configuration instead of replacing all pg_hba file

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,5 +1,10 @@
 set -e
 
-sudo cp .travis/pg_hba.conf $(psql -U postgres -c "SHOW hba_file" -At)
+AUTH_FILE=`psql -U postgres -c "SHOW hba_file" -At`
+
+# md5 authentication
+
+sudo sed -i '1s/^/host	all	crystal_md5	127.0.0.1\/32	md5\n/' ${AUTH_FILE}
+sudo sed -i '2s/^/host	all	crystal_md5	::1\/128	md5\n/' ${AUTH_FILE}
 
 sudo service postgresql restart $TRAVIS_POSTGRESQL_VERSION
