15a9a64399a97ce35aa3825b08a844c2dc66612f
greenbigfrog <greenbigfrog@gmail.com>
2018-02-05 17:41:25 +0100

    Remove comment

--- src/pg_ext/big_decimal.cr
+++ src/pg_ext/big_decimal.cr
@@ -5,8 +5,6 @@ module PG
     # Returns a BigDecimal representation of the numeric. This retains all precision.
     def to_big_d
       return BigDecimal.new("0") if nan? || ndigits == 0
-
-      # Since BigDecimal allows initialaztion from String, why should one reinvent the wheel?
       BigDecimal.new(to_s)
     end
   end
