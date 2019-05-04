3a551660744a5b8a3c4dd9c7cbd6e28f494d350b
Vlad Faust <mail@vladfaust.com>
2019-05-04 12:27:31 +0300

    Add `Enumerable` channels overload for `PG.connect_listen`
    Closes #166

--- src/pg.cr
+++ src/pg.cr
@@ -7,18 +7,33 @@ module PG
     DB.open(url)
   end
 
-  # Establish a special listen connection to the database
+  # Establish a special listen-only connection to the database.
+  #
+  # ```
+  # PG.connect_listen(ENV["DATABASE_URL"], "foo", "bar") do |notification|
+  #   pp notification.channel, notification.payload, notification.pid
+  # end
+  # ```
   def self.connect_listen(url, *channels : String, &blk : PQ::Notification ->) : ListenConnection
-    ListenConnection.new(url, *channels, &blk)
+    connect_listen(url, channels, &blk)
+  end
+
+  # ditto
+  def self.connect_listen(url, channels : Enumerable(String), &blk : PQ::Notification ->) : ListenConnection
+    ListenConnection.new(url, channels, &blk)
   end
 
   class ListenConnection
     @conn : PG::Connection
 
-    def initialize(url, *channels : String, &blk : PQ::Notification ->)
+    def self.new(url, *channels : String, &blk : PQ::Notification ->)
+      new(url, channels, &blk)
+    end
+
+    def initialize(url, channels : Enumerable(String), &blk : PQ::Notification ->)
       @conn = DB.connect(url).as(PG::Connection)
       @conn.on_notification(&blk)
-      @conn.listen(*channels)
+      @conn.listen(channels)
     end
 
     # Close the connection.
