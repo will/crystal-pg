1632dffafed28d3a660a4cb9b667d6336e447ab0
Mike Fiedler <miketheman@gmail.com>
2019-10-09 18:31:52 -0400

    test: add negative test range
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

--- spec/pg/decoders/range_decoder_spec.cr
+++ spec/pg/decoders/range_decoder_spec.cr
@@ -6,5 +6,6 @@ describe PG::Decoders do
     test_decode "exclusive", "'(10,20]'::int4range", Range.new(11, 21)
     test_decode "inclusive", "'[10,20]'::int4range", Range.new(10, 21)
     test_decode "inclusive", "'[10,20)'::int4range", Range.new(10, 20)
+    test_decode "negatives", "'[-14,-5)'::int4range", Range.new(-14, -5)
   end
 end
