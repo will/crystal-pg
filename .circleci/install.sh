cafe0f217921ae4e369a9fec3a02e6bff5a6e26f
Will Leinweber <will@bitfission.com>
2019-12-16 15:44:03 -0800

    ci: try to future proof pdgd repo names

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -1,9 +1,9 @@
 set -e
 
 apt-get update
-apt-get install curl -y
+apt-get install curl lsb-release -y
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 apt-get update
 apt-get install $CIRCLE_JOB -y
 
