493367964afda5b97e424f209dd743a4ec19cbd3
Pascal Betz <pascal.betz@swisscom.com>
2020-10-04 00:40:58 +0200

    use correct values for flags

--- src/pg/decoders/range_decoder.cr
+++ src/pg/decoders/range_decoder.cr
@@ -8,7 +8,7 @@ module PG
       DECODERS_TO_OID = {
         "Int32" => [3904],
         "Int64" => [3926],
-        "Time" => [
+        "Time"  => [
           3912,
           3910,
           1114,
@@ -19,28 +19,29 @@ module PG
 
       # Range OID => OID of upper/lower boundary
       OIDS_TO_SUBOIDS = {
-        3904 => 23,       # int4range
-        3926 => 20,       # int8range
-        3912 => 1082,     # daterange
-        3910 => 1114,     # tstzrange
-        3908 => 1114,     # tsrange,
-        3906 => 1700,     # numrange
+        3904 => 23,   # int4range
+        3926 => 20,   # int8range
+        3912 => 1082, # daterange
+        3910 => 1114, # tstzrange
+        3908 => 1114, # tsrange,
+        3906 => 1700, # numrange
       }
 
       getter oids : Array(Int32)
 
-      FLAG_EMPTY            = 0b00000001
-      FLAG_LOWER_INCLUSIVE  = 0b00000010
-      FLAG_LOWER_INFINITY   = 0b00000100
-      FLAG_UPPER_INCLUSIVE  = 0b00001000
-      FLAG_UPPER_INFINITY   = 0b00010000
+      # See https://github.com/postgres/postgres/blob/5cbfce562f7cd2aab0cdc4694ce298ec3567930e/src/include/utils/rangetypes.h#L36
+      FLAG_EMPTY           = 0b00000001
+      FLAG_LOWER_INCLUSIVE = 0b00000010
+      FLAG_UPPER_INCLUSIVE = 0b00000100
+      FLAG_LOWER_INFINITY  = 0b00001000
+      FLAG_UPPER_INFINITY  = 0b00010000
 
       def initialize(@oids : Array(Int32))
       end
 
       {% for key, value in DECODERS_TO_OID %}
-      private def decode_boundary(io, oid, header, type : {{ key.id }}.class )
-        if header.empty
+      private def decode_boundary(io, oid, infinity, type : {{ key.id }}.class )
+        if infinity
           \{% if T.nilable? %}
           nil
           \{% else %}
@@ -80,8 +81,8 @@ module PG
         if header.empty
           empty_range(T)
         else
-          lower = decode_boundary(io, oid, header, T)
-          upper = decode_boundary(io, oid, header, T)
+          lower = decode_boundary(io, oid, header.lower_infinity, T)
+          upper = decode_boundary(io, oid, header.upper_infinity, T)
           Range.new(lower, upper, !header.upper_inclusive)
         end
       end
