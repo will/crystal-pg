cafe0003d01ba0736543b60a46fc4160454be244
Will Leinweber <will@bitfission.com>
2021-10-21 18:02:33 -0700

    Apparenlty gnupg is now required for apt-key add ?

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -1,7 +1,7 @@
 set -e
 
 apt-get update
-apt-get install curl lsb-release -y
+apt-get install gnupg curl lsb-release -y
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 apt-get update
