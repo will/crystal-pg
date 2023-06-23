ef41f768e78fdb821e4382815c8d9be96f403b07
Brian J. Cardiff <bcardiff@gmail.com>
2023-06-23 15:34:34 -0300

    Update shards.nix

--- shards.nix
+++ shards.nix
@@ -1,7 +1,7 @@
 {
   db = {
     url = "https://github.com/crystal-lang/crystal-db.git";
-    rev = "v0.11.0";
-    sha256 = "1ylfhpn64p72ywi39niqb179f61z08q4qd4hhjza05z18mdaghl3";
+    rev = "v0.12.0";
+    sha256 = "1in8w2dz7nlhqgc9l6b3pi6f944m29nhbg3p5j40qzvsrr8lqaj7";
   };
 }
