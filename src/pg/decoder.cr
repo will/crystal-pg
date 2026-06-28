c0eb342a74ec21f78d576d92f99b13b4edc9f857
Anton Maminov <anton.linux@gmail.com>
2026-06-28 11:14:18 +0300

    Update src/pg/decoder.cr
    Co-authored-by: Jamie Gaskins <jgaskins@hey.com>

--- src/pg/decoder.cr
+++ src/pg/decoder.cr
@@ -2,7 +2,26 @@ require "json"
 require "uuid"
 
 module PG
-  alias PGValue = String | Nil | Bool | Int32 | Float32 | Float64 | Time | JSON::Any | PG::Numeric | UUID | PG::Range(Int32) | PG::Range(Int64) | PG::Range(Time) | PG::Range(PG::Numeric) | Array(PG::Range(Int32)) | Array(PG::Range(Int64)) | Array(PG::Range(Time)) | Array(PG::Range(PG::Numeric))
+  alias PGValue = Union(
+    String,
+    Nil,
+    Bool,
+    Int32,
+    Float32,
+    Float64,
+    Time,
+    JSON::Any,
+    PG::Numeric,
+    UUID,
+    PG::Range(Int32),
+    PG::Range(Int64),
+    PG::Range(Time),
+    PG::Range(PG::Numeric),
+    Array(PG::Range(Int32)),
+    Array(PG::Range(Int64)),
+    Array(PG::Range(Time)),
+    Array(PG::Range(PG::Numeric)),
+  )
 
   # :nodoc:
   module Decoders
